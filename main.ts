441a2c812255af0bc8c54da7990d5bc822d8e57b
Vovs <74075592+bilingual-vovs@users.noreply.github.com>
2023-03-13 07:08:11 +0200

    Update

--- main.ts
+++ main.ts
@@ -1,10 +1,20 @@
-function init() {
-    return {
-        main: () => {
-            player.say(2022)
-        }
+/**
+ * Pick some fruit and peel it.
+ */
+//% weight=70 icon="\uf185" color=#EC7505
+namespace tropic {
+    /**
+     * Pick a fruit
+     */
+    //% blockId=tropic_pick block="pick a %fruit"
+    export function pick(fruit: TropicalFruit): boolean {
+        return true;
     }
-}
-function test(){
-    player.say(2023)
+    /**
+     * Peel the fruit if possible
+     */
+    //% blockId=tropic_peel block="peel a %fruit"
+    export function peel(fruit: TropicalFruit): boolean {
+        return (fruit == TropicalFruit.Banana);
+    }
 }
